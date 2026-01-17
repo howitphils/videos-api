dfdb28782e9d9cee3c3a0a083f0a0c84c90fcea4
howitphils <uladzislauhr@gmail.com>
2026-01-17 07:28:05 +0300

    fixed ts config file

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,6 @@
 import express from "express";
-const app = express();
-const port = 3003;
-
-enum Resolutions {
-  P144,
-  P240,
-  P360,
-  P480,
-  P720,
-  P1080,
-  P1440,
-  P2160,
-}
-
-type VideoType = {
-  id: number;
-  title: string;
-  author: string;
-  canBeDownloaded: boolean;
-  minAgeRestriction: number | null;
-  createdAt: string;
-  publicationDate: string;
-  availableResolutions: Resolutions;
-};
+export const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
-app.get("/hi", (req, res) => {
-  res.send("heheo, welcome back to back, my friend");
 });
