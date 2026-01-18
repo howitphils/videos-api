0e6154e8608bb2645582a93fc55065f84ca38e9d
howitphils <uladzislauhr@gmail.com>
2026-01-18 09:14:29 +0300

    added dotenv config

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { videosRouter } from "./routers/videos-router";
 import { testingRouter } from "./routers/testing-router";
+import { configDotenv } from "dotenv";
 
 export const app = express();
+
+configDotenv();
 
 app.use(express.json());
 app.use("/videos", videosRouter);
